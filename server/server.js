78870851e9c24ee397e70357ccf739ded4cd03e7
Vaibhavi Gandhi <vaibhavimgandhi@gmail.com>
2024-01-21 00:19:30 +0530

    created server and client utilities

---
+++ server/server.js
@@ -0,0 +1,30 @@
+// server.js
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors"); 
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// routes
+const todo = require("./routes/todo"); // added
+
+// connect database
+connectDB();
+
+//cors
+app.use(cors({ origin: true, credentials: true })); 
+
+// initialize middleware
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("Server up and running"));
+
+// use routes
+app.use("/api/todo", todo); // added
+// setting up port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
